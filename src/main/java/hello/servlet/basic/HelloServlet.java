94d39389850bca42f80fa4634e4fffb28bb5080f
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-06-23 15:35:30 +0900

    feat : hello서블릿

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,41 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+//서블릿은 HttpServlet를 상속받고 시작해야된다.
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        //contentType와 characterEncoding을 설정해준다.
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+
+        response.getWriter().write("hello" + username);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
