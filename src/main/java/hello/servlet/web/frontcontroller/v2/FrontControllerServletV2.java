2f8e013b29d08a589d3587ba4ba93a209c226014
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-07-06 14:16:46 +0900

    refactor : View 분리-v2

---
+++ src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -0,0 +1,52 @@
+package hello.servlet.web.frontcontroller.v2;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+//frontController가 하는 역할이 uri 정보에 따라 controller를 매핑시켜주는 역할을 한다.
+@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
+public class FrontControllerServletV2 extends HttpServlet {
+
+    //key : url, value : Controller
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV2() {
+        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV2.service");
+
+        //클라이언트로 부터 입력된 uri 정보를 받아온다.
+        String requestURI = request.getRequestURI();
+
+        //controllerMap으로 클라이언트로 부터 입력된 uri 정보를 통해 controller를 매핑.
+        ControllerV2 controller = controllerMap.get(requestURI);
+
+        //controller가 매핑되지 못했을 경우를 대비해서 만들어줌
+        if(controller == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        //이전과 다르게 process함수를 수행하고 나면 jsp uri 주소를 반환한다.
+        MyView view = controller.process(request, response);
+        view.render(request,response);
+    }
+
+
+}
