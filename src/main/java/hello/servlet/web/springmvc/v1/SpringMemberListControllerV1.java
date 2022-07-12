3a935432133367dc5f6f5fc79d2693ae48c24c48
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-07-12 14:39:50 +0900

    fix : springmvc-v1 필요없는 코드 삭제

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
-import java.util.Map;
 
 @Controller
 public class SpringMemberListControllerV1 {
