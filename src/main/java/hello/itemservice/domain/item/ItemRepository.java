c2d256ad1d0a74dbae4d05f405f8066e40f90013
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-07-14 20:48:56 +0900

    feat : 웹페이지 만들기(상품 도메인 개발)

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,43 @@
+package hello.itemservice.domain.item;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>(); //static로 선언
+    private static long sequence = 0L; //static로 선언
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Item> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long id, Item updateParam){
+        Item findItem = findById(id);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+
+}
