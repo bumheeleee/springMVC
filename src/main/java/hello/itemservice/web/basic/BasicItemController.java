249fde628e6bb540b1fda338363cb8ce54b2fbb6
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-07-22 14:26:12 +0900

    chore : PRG Post/Redirect/Get - 저장하거나 업데이트를 하고 새로고침을 하면 발생할 수 있는 오류에 대해 해결함

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -43,69 +43,20 @@ public class BasicItemController {
     }
 
 
-    //상품 등록V1
-    //@PostMapping("/add")
-    public String addItemV1(@RequestParam String itemName,
-                       @RequestParam int price,
-                       @RequestParam Integer quantity,
-                       Model model){
-
-        Item item = new Item();
-        item.setItemName(itemName);
-        item.setPrice(price);
-        item.setQuantity(quantity);
-
-        itemRepository.save(item);
-
-        model.addAttribute("item", item);
-
-        return "/basic/item";
-    }
-
     /**
-     @ModelAttribute의 기능
-
-     @ModelAttribute - 요청 파라미터 처리
-     @ModelAttribute 는 Item 객체를 생성하고, 요청 파라미터의 값을 프로퍼티 접근법(setXxx)으로
-     입력해준다.
-
-     @ModelAttribute - Model 추가
-     @ModelAttribute 는 중요한 한가지 기능이 더 있는데, 바로 모델(Model)에 @ModelAttribute 로
-     지정한 객체를 자동으로 넣어준다. 지금 코드를 보면 model.addAttribute("item", item) 가 주석처리
-     되어 있어도 잘 동작하는 것을 확인할 수 있다.
+     * form형식으로 저장하거나 update 기능을 수행하는 controller는 redirect를 해야한다.
+     * 왜냐하면 서버에 마지막으로 요청한 url이 남아있어서 새로고침을 하면
+     * update하거나 save하는 행위가 계속 반복될 수 있다.
+     * 따라서 다른 url로 redirect를 해줘야 된다.
      */
-    //상품 등록V2
-    //@PostMapping("/add")
-    public String addItemV2(@ModelAttribute("item") Item item, Model model){
-
-        itemRepository.save(item);
-//      model.addAttribute("item", item); ModelAttribute("item")의 "item"으로 model에 자동으로 추가되서,생략 가능
-
-        return "/basic/item";
-    }
-
-
-    //상품 등록V3
-    //@ModelAttribute의 ()도 생략가능
-    //생략하면 객체 클래스에 첫글자만 소문자로 바꿔서, 대입됨
-    //ex) Item -> item
-    //@PostMapping("/add")
-    //이렇게 사용하기 : 시인성에 더욱 좋다.
-    public String addItemV3(@ModelAttribute Item item){
-        itemRepository.save(item);
-        return "/basic/item";
-    }
-
-
-    //상품 등록V4
-    //@ModelAttribute도 생략이 가능하다.
+    //상품 등록(저장)
     @PostMapping("/add")
-    public String addItemV4(Item item){
+    public String add(@ModelAttribute Item item){
         itemRepository.save(item);
-        return "/basic/item";
+        return "redirect:/basic/items/" + item.getId();
     }
 
-
+    //상품 수정(form만 보여줌)
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable long itemId, Model model){
         Item item = itemRepository.findById(itemId);
@@ -114,7 +65,13 @@ public class BasicItemController {
         return "/basic/editForm";
     }
 
-
+    /**
+     * form형식으로 저장하거나 update 기능을 수행하는 controller는 redirect를 해야한다.
+     * 왜냐하면 서버에 마지막으로 요청한 url이 남아있어서 새로고침을 하면
+     * update하거나 save하는 행위가 계속 반복될 수 있다.
+     * 따라서 다른 url로 redirect를 해줘야 된다.
+     */
+    //상품 수정(수정하는 기능)
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable long itemId, @ModelAttribute Item item){
         itemRepository.update(itemId, item);
